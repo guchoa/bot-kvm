a6aa09cbefcb45d9258b1fdf3f7f7092d4bbc5a9
guchoa <gabic812@gmail.com>
2025-07-06 15:47:21 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,10 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True 
+intents.presences = True
+intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 CLASSES_EMOJIS = {
     'sacerdote': '🟡',
